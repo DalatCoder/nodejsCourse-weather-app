cc6b1067ac0267fe8d6a34b7236c1b8b217fb5ba
DalatCoder <hieuntctk42@gmail.com>
2019-06-16 18:08:42 +0700

    Send HTML and JSON

--- src/app.js
+++ src/app.js
@@ -3,19 +3,23 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.end('Hello express!')
+  res.send('<h1>Hello express</h1>')
 })
 
 app.get('/help', (req, res) => {
-  res.end('Help page.')
+  res.send('<h1>Help page.</h1>')
 })
 
 app.get('/about', (req, res) => {
-  res.end('About page.')
+  res.send('<h1>About page.</h1>')
 })
 
 app.get('/weather', (req, res) => {
-  res.end('Weather page.')
+  res.send({
+    location: 'Da Lat',
+    temperature: 24.4,
+    rainPercent: 50
+  })
 })
 
 app.listen(3000, () => {
