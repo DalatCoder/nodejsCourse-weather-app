a4b8f02d220aa4828acb787ac4dccbc6d7024734
DalatCoder <hieuntctk42@gmail.com>
2019-06-16 16:39:05 +0700

    Hello express

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.end('Hello express!')
+})
+
+app.get('/help', (req, res) => {
+  res.end('Help page!')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running at port 3000')
+})
