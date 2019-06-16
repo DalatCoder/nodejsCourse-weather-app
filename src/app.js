16f426ea0f8815234ff966f54e7949d91321be36
DalatCoder <hieuntctk42@gmail.com>
2019-06-16 21:36:25 +0700

    Customize hbs

--- src/app.js
+++ src/app.js
@@ -3,9 +3,34 @@ const express = require('express')
 
 const app = express()
 
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
 
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    name: 'Trong Hieu',
+    title: 'Weather app'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Trong Hieu'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
