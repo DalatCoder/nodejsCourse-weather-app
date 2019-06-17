7ec28ff4e856569a93ef6ec7697d14d307c4c89c
DalatCoder <hieuntctk42@gmail.com>
2019-06-17 10:27:41 +0700

    Fetch data from server

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
 console.log('Client javascript is loaded!')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+  response.json().then((data) => {
+    console.log(data)
+  })
+})
+
+const address = 'Ho Chi Minh'
+fetch(`/weather?address=${encodeURIComponent(address)}`).then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error)
+    } else {
+      console.log(data.location)
+      console.log(data.forecast)
+    }
+  })
+})
