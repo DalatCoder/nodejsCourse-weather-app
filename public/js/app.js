3781036819e276071de7087629869af7bf0079e4
DalatCoder <hieuntctk42@gmail.com>
2019-06-17 10:55:15 +0700

    Create input field, fetch data from server

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,23 @@
 console.log('Client javascript is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-  response.json().then((data) => {
-    console.log(data)
-  })
-})
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-const address = 'Ho Chi Minh'
-fetch(`/weather?address=${encodeURIComponent(address)}`).then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data.location)
-      console.log(data.forecast)
-    }
-  })
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value
+
+  fetch(`/weather?address=${encodeURIComponent(location)}`)
+    .then((response) => {
+      response.json()
+        .then((data) => {
+          if (data.error) {
+            console.log(data.error)
+          } else {
+            console.log(data.location)
+            console.log(data.forecast)
+          }
+        })
+    })
 })
