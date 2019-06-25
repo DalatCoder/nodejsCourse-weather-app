20c44bf5369dea0d99816eb456112aecf47c9b69
DalatCoder <hieuntctk42@gmail.com>
2019-06-25 22:55:41 +0700

    Async/await client javascript

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ const search = document.querySelector('input')
 const messageOne = document.getElementById('message-1')
 const messageTwo = document.getElementById('message-2')
 
-weatherForm.addEventListener('submit', (e) => {
+weatherForm.addEventListener('submit', async (e) => {
   e.preventDefault()
 
   const location = search.value
@@ -14,16 +14,13 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
 
-  fetch(`/weather?address=${encodeURIComponent(location)}`)
-    .then((response) => {
-      response.json()
-        .then((data) => {
-          if (data.error) {
-            messageOne.textContent = data.error
-          } else {
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-          }
-        })
-    })
+  const temp = await fetch(`/weather?address=${encodeURIComponent(location)}`)
+  const data = await temp.json()
+
+  if (data.error) {
+    return messageOne.textContent = data.error
+  }
+
+  messageOne.textContent = data.location
+  messageTwo.textContent = data.forecast
 })
